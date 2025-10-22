2b2cfa74d4ddd96961353c436294133898cca057
arthurMoreiraC <a2024951729@teiacoltec.org>
2025-10-22 11:49:29 -0300

    Menu meu e do marco
    Added authorship information and print statements for menu options.

---
+++ Menu.py
@@ -0,0 +1,11 @@
+#Arthur Moreira & Marco Leão 205B
+
+print("1- questao")
+print("2- questao")
+questao = int(input("digite a questão: "))
+
+match questao:
+    case 1:
+        print("balls")
+    case 2:
+        print("bolas")
